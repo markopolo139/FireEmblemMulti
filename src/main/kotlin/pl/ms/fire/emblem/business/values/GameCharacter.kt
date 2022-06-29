67156efbf843c4cd4175ddb0d619f0f63cce024d
maropolo139 <marekseget@onet.pl>
2022-06-29 15:51:13 +0200

    feat(character): on init sort skills by priority

--- src/main/kotlin/pl/ms/fire/emblem/business/values/GameCharacter.kt
+++ src/main/kotlin/pl/ms/fire/emblem/business/values/GameCharacter.kt
@@ -15,6 +15,11 @@ class GameCharacter(
     val characterClass: CharacterClass,
     val moved: Boolean
 ) {
+
+    init {
+        skills.sortedByDescending { it.priority }
+    }
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
