9da149c263f88d380ae8417e62409fd3f1eba711
maropolo139 <marekseget@onet.pl>
2022-06-29 16:02:35 +0200

    feat(exceptions): create excetpions for characterPair

---
+++ src/main/kotlin/pl/ms/fire/emblem/business/exceptions/NoSupportCharacterException.kt
@@ -0,0 +1,3 @@
+package pl.ms.fire.emblem.business.exceptions
+
+class NoSupportCharacterException: BusinessException("Pair does not have support character")
---
+++ src/main/kotlin/pl/ms/fire/emblem/business/exceptions/PairAlreadyHaveSupportException.kt
@@ -0,0 +1,3 @@
+package pl.ms.fire.emblem.business.exceptions
+
+class PairAlreadyHaveSupportException: BusinessException("Pair can't join with another unit, support already exists")
