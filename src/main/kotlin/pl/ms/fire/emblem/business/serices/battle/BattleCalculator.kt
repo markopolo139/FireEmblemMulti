49aeac7507345e8e93cf21fc69ac71ebcabdc2e0
maropolo139 <marekseget@onet.pl>
2022-06-30 16:57:31 +0200

    refactor(battleCalc): delete random number

--- src/main/kotlin/pl/ms/fire/emblem/business/serices/battle/BattleCalculator.kt
+++ src/main/kotlin/pl/ms/fire/emblem/business/serices/battle/BattleCalculator.kt
@@ -3,5 +3,5 @@ package pl.ms.fire.emblem.business.serices.battle
 import pl.ms.fire.emblem.business.entities.CharacterPair
 
 fun interface BattleCalculator {
-    fun calcDmg(playerPair: CharacterPair, enemyPair: CharacterPair, randomNumber: Int): Int
+    fun calcDmg(playerPair: CharacterPair, enemyPair: CharacterPair): Int
 }
--- src/main/kotlin/pl/ms/fire/emblem/business/values/character/OffensiveSkill.kt
+++ src/main/kotlin/pl/ms/fire/emblem/business/values/character/OffensiveSkill.kt
@@ -9,37 +9,37 @@ enum class OffensiveSkill(
     override val percentCalc: (Int) -> Int
 ): Skill, BattleCalculator {
     AETHER(Stat.SKILL, 5, {stat -> stat/2}) {
-        override fun calcDmg(playerPair: CharacterPair, enemyPair: CharacterPair, randomNumber: Int): Int {
+        override fun calcDmg(playerPair: CharacterPair, enemyPair: CharacterPair): Int {
             TODO("Not yet implemented")
         }
     },
     IGNIS(Stat.SKILL, 5, {stat -> stat}) {
-        override fun calcDmg(playerPair: CharacterPair, enemyPair: CharacterPair, randomNumber: Int): Int {
+        override fun calcDmg(playerPair: CharacterPair, enemyPair: CharacterPair): Int {
             TODO("Not yet implemented")
         }
     },
     LUNA(Stat.SKILL, 3, {stat -> stat}) {
-        override fun calcDmg(playerPair: CharacterPair, enemyPair: CharacterPair, randomNumber: Int): Int {
+        override fun calcDmg(playerPair: CharacterPair, enemyPair: CharacterPair): Int {
             TODO("Not yet implemented")
         }
     },
     ASTRA(Stat.SKILL, 4, {stat -> stat/2}) {
-        override fun calcDmg(playerPair: CharacterPair, enemyPair: CharacterPair, randomNumber: Int): Int {
+        override fun calcDmg(playerPair: CharacterPair, enemyPair: CharacterPair): Int {
             TODO("Not yet implemented")
         }
     },
     SOL(Stat.SKILL, 3, {stat -> stat}) {
-        override fun calcDmg(playerPair: CharacterPair, enemyPair: CharacterPair, randomNumber: Int): Int {
+        override fun calcDmg(playerPair: CharacterPair, enemyPair: CharacterPair): Int {
             TODO("Not yet implemented")
         }
     },
     LETHALITY(Stat.SKILL, 5, {stat -> stat/4}) {
-        override fun calcDmg(playerPair: CharacterPair, enemyPair: CharacterPair, randomNumber: Int): Int {
+        override fun calcDmg(playerPair: CharacterPair, enemyPair: CharacterPair): Int {
             TODO("Not yet implemented")
         }
     },
     VENGEANCE(Stat.SKILL, 5, {stat -> stat * 2}) {
-        override fun calcDmg(playerPair: CharacterPair, enemyPair: CharacterPair, randomNumber: Int): Int {
+        override fun calcDmg(playerPair: CharacterPair, enemyPair: CharacterPair): Int {
             TODO("Not yet implemented")
         }
     },;
