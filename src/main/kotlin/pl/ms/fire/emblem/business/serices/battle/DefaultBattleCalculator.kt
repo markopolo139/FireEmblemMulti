06dc72ed039649c43805444c062283158f3f9184
markopolo139 <marekseget@onet.pl>
2022-07-10 20:55:01 +0200

    feat(BattleCalc): create default battle calculator

---
+++ src/main/kotlin/pl/ms/fire/emblem/business/serices/battle/DefaultBattleCalculator.kt
@@ -0,0 +1,25 @@
+package pl.ms.fire.emblem.business.serices.battle
+
+import pl.ms.fire.emblem.business.entities.CharacterPair
+import pl.ms.fire.emblem.business.utlis.BattleUtils
+import pl.ms.fire.emblem.business.utlis.getStat
+import pl.ms.fire.emblem.business.values.category.AttackCategory
+import pl.ms.fire.emblem.business.values.character.Stat
+
+class DefaultBattleCalculator: BattleCalculator {
+    override fun calcDmg(playerPair: CharacterPair, enemyPair: CharacterPair): Int {
+
+        val playerEquippedItem = playerPair.leadCharacter.equipment[playerPair.leadCharacter.currentEquippedItem]
+        val playerAttack = playerPair.battleStat.attack
+        val opponentDefense = if (playerEquippedItem.attackCategory == AttackCategory.MAGICAL) {
+            enemyPair.boostedStats.getStat(Stat.RESISTANCE)
+        } else {
+            enemyPair.boostedStats.getStat(Stat.DEFENSE)
+        }
+
+        return BattleUtils.criticalCheck(
+            playerPair.battleStat.critical, enemyPair.boostedStats.getStat(Stat.LUCK),playerAttack - opponentDefense
+        )
+
+    }
+}
