d2329e3301be998068d3b03c481c372d51dd9daf
markopolo139 <marekseget@onet.pl>
2022-07-20 22:57:53 +0200

    feat(BattleUtils): create check for double attack

--- src/main/kotlin/pl/ms/fire/emblem/business/utlis/BattleUtils.kt
+++ src/main/kotlin/pl/ms/fire/emblem/business/utlis/BattleUtils.kt
@@ -1,6 +1,18 @@
 package pl.ms.fire.emblem.business.utlis
 
+import pl.ms.fire.emblem.business.entities.CharacterPair
+import pl.ms.fire.emblem.business.values.character.Stat
+
 object BattleUtils {
     fun criticalCheck(percent: Int, enemyLuck: Int, dmgDealt: Int) =
         if (RandomSingleton.random.nextInt(1, 101) <= (percent - enemyLuck)) dmgDealt * 3 else dmgDealt
+
+    fun hitCheck(hitPercent: Int, enemyAvo: Int) =
+        RandomSingleton.random.nextInt(1, 101) <= (hitPercent - enemyAvo)
+
+    fun skillCheck(percent: Int) = RandomSingleton.random.nextInt(1, 101) <= percent
+
+    fun isDoubleAttack(attackerPair: CharacterPair, defenderPair: CharacterPair): Boolean =
+        attackerPair.boostedStats.getStat(Stat.SPEED) - defenderPair.boostedStats.getStat(Stat.SPEED) >= 5
+
 }
