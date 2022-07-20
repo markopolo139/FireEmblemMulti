03c61f342aa7e443304fc56338bb2891976af49e
markopolo139 <marekseget@onet.pl>
2022-07-20 22:58:44 +0200

    feat(Terrain): add avoid boost to terrain

--- src/main/kotlin/pl/ms/fire/emblem/business/values/board/Terrain.kt
+++ src/main/kotlin/pl/ms/fire/emblem/business/values/board/Terrain.kt
@@ -2,13 +2,17 @@ package pl.ms.fire.emblem.business.values.board
 
 import pl.ms.fire.emblem.business.utlis.Displayable
 
-enum class Terrain(val dmgReduction: Int, val movementReduction: Int): Displayable {
-    GRASS(0,1),
-    PLAIN(0,1),
-    FORREST(2,2),
-    MOUNTAIN(3,4),
-    FORTRESS(5, 3),
-    SAND(0, 3);
+enum class Terrain(
+    val dmgReduction: Int,
+    val avoidBoost: Int,
+    val movementReduction: Int
+): Displayable {
+    GRASS(0,5,1),
+    PLAIN(0,0,1),
+    FORREST(2,30,2),
+    MOUNTAIN(3,40,4),
+    FORTRESS(5,20, 3),
+    SAND(0,-10, 3);
 
     override val displayName: String
         get() = name
