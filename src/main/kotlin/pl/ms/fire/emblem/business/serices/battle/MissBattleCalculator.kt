e9f158fb8a97140bed171127cca2c40cccb98798
markopolo139 <marekseget@onet.pl>
2022-07-20 22:57:24 +0200

    feat(battle): create calculator for missed attack

---
+++ src/main/kotlin/pl/ms/fire/emblem/business/serices/battle/MissBattleCalculator.kt
@@ -0,0 +1,9 @@
+package pl.ms.fire.emblem.business.serices.battle
+
+import pl.ms.fire.emblem.business.entities.CharacterPair
+
+class MissBattleCalculator: BattleCalculator {
+    override fun calcDmg(playerPair: CharacterPair, enemyPair: CharacterPair): Int {
+        return 0
+    }
+}
