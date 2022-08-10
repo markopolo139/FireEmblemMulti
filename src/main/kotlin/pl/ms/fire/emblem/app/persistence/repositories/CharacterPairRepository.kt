456ab474baafc6e5626ecd15ecfba249c78fe64f
maropolo139 <marekseget@onet.pl>
2022-08-10 17:52:04 +0200

    feat(repo): create character pair repository

---
+++ src/main/kotlin/pl/ms/fire/emblem/app/persistence/repositories/CharacterPairRepository.kt
@@ -0,0 +1,29 @@
+package pl.ms.fire.emblem.app.persistence.repositories
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import org.springframework.stereotype.Repository
+import pl.ms.fire.emblem.app.persistence.entities.CharacterPairEntity
+
+@Repository
+interface CharacterPairRepository: JpaRepository<CharacterPairEntity, Int> {
+
+    @Query(
+        "select * from character_pairs where lead_character_id in (" +
+                "select game_character_id from game_characters where" +
+                "preset_id in (select preset_id from player_character_presets where player_id = :playerId)" +
+        ")", nativeQuery = true
+    )
+    fun getAllPlayerCharacters(@Param("playerId") playerId: Int): Set<CharacterPairEntity>
+
+    fun getOnlyNotMovedCharacters(playerId: Int) =
+        getAllPlayerCharacters(playerId).filter { !it.leadCharacter.moved }.toSet()
+
+    fun getOnlyAliveCharacters(playerId: Int) =
+        getAllPlayerCharacters(playerId).filter { it.leadCharacter.remainingHp != 0 }.toSet()
+
+    fun getAliveAndNotMovedCharacters(playerId: Int) =
+        getOnlyNotMovedCharacters(playerId).filter { !it.leadCharacter.moved }.toSet()
+
+}
