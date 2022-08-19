db878529ea2cb9ab31e8455bb7765927e01fc671
maropolo139 <marekseget@onet.pl>
2022-08-19 14:58:29 +0200

    refactor(controllers): rename test to authentication controller

--- src/main/kotlin/pl/ms/fire/emblem/web/controllers/AuthenticationController.kt
+++ src/main/kotlin/pl/ms/fire/emblem/web/controllers/AuthenticationController.kt
@@ -5,27 +5,14 @@ import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import pl.ms.fire.emblem.app.recovery.PasswordRecoveryService
 import pl.ms.fire.emblem.app.services.AuthenticationService
-import pl.ms.fire.emblem.web.utils.getServerAddress
-import javax.servlet.http.HttpServletRequest
-import kotlin.math.log
 
 @RestController
 @CrossOrigin
-class Test {
-
+class AuthenticationController {
 
     @Autowired
     private lateinit var authenticationService: AuthenticationService
-
-    @Autowired
-    private lateinit var passwordRecoveryService: PasswordRecoveryService
-
-    @GetMapping("/api/v1/test")
-    fun test(httpServletRequest: HttpServletRequest, @RequestParam("email") email: String) {
-        passwordRecoveryService.sendMessage(email, getServerAddress(httpServletRequest))
-    }
 
     @GetMapping("/auth")
     fun auth(@RequestParam("login") login: String, @RequestParam("password") password: String): String {
