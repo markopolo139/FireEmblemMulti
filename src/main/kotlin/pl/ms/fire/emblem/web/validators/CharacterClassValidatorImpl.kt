512d28fe7d68f5fd5786224f30795a8be7b80d59
maropolo139 <marekseget@onet.pl>
2022-08-21 14:55:38 +0200

    refactor(characterClassValidator): fix typoos

--- src/main/kotlin/pl/ms/fire/emblem/web/validators/CharacterClassValidatorImpl.kt
+++ src/main/kotlin/pl/ms/fire/emblem/web/validators/CharacterClassValidatorImpl.kt
@@ -1,12 +1,13 @@
 package pl.ms.fire.emblem.web.validators
 
+import pl.ms.fire.emblem.business.values.character.CharacterClass
 import javax.validation.ConstraintValidator
 import javax.validation.ConstraintValidatorContext
 
 class CharacterClassValidatorImpl: ConstraintValidator<CharacterClassValidator, String> {
     override fun isValid(value: String?, context: ConstraintValidatorContext?): Boolean {
         return try {
-            CharacterClass.valueOf(valie ?: "")
+            CharacterClass.valueOf(value ?: "")
             true
         }
         catch(e: Exception) {
