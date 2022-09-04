091f25504957eff72f2fce78f1b4398a0c74c999
maropolo139 <marekseget@onet.pl>
2022-09-04 18:54:57 +0200

    feat(SpotRepository): change to optional return type

--- src/main/kotlin/pl/ms/fire/emblem/app/persistence/repositories/SpotRepository.kt
+++ src/main/kotlin/pl/ms/fire/emblem/app/persistence/repositories/SpotRepository.kt
@@ -3,11 +3,12 @@ package pl.ms.fire.emblem.app.persistence.repositories
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import pl.ms.fire.emblem.app.persistence.entities.SpotEntity
+import java.util.Optional
 
 @Repository
 interface SpotRepository: JpaRepository<SpotEntity, Int> {
 
-    fun getByBoardIdAndXAndY(boardId: Int, x: Int, y: Int): SpotEntity
+    fun getByBoardIdAndXAndY(boardId: Int, x: Int, y: Int): Optional<SpotEntity>
 
     fun getAllByBoardId(boardId: Int): Set<SpotEntity>
 
